a0129832340e44f0c5a2b070e77218f371850c1e
Shevchenko Andrey <demorest49de@gmail.com>
2024-02-17 14:46:25 +0300

    Changes Ten Green Bottles.ts

--- src/tests/Ten Green Bottles/Ten Green Bottles.test.ts
+++ src/tests/Ten Green Bottles/Ten Green Bottles.test.ts
@@ -1,20 +1,26 @@
 import {tenGreenBottles} from './Ten Green Bottles'
 
-test('toCamelCase', () => {
+test('Ten Green Bottles', () => {
 
-    const expect2 = `
-Two green bottles hanging on the wall,
+const expect2 =
+`Two green bottles hanging on the wall,
 Two green bottles hanging on the wall,
 And if one green bottle should accidentally fall,
-There'll be two green bottles hanging on the wall.     
-    `
-    const expect1 = `
+There'll be one green bottle hanging on the wall.
+
 One green bottle hanging on the wall,
+One green bottle hanging on the wall,
+If that one green bottle should accidentally fall,
+There'll be no green bottles hanging on the wall.
+`
+
+const expect1 =
+`One green bottle hanging on the wall,
 One green bottle hanging on the wall,
 And if that one green bottle should accidentally fall,
-There'll be no green bottles hanging on the wall.     
-    `
+There'll be no green bottles hanging on the wall.`
 
+    // expect(expect2).toStrictEqual(tenGreenBottles(3))
     expect(expect2).toStrictEqual(tenGreenBottles(2))
-    expect(expect1).toStrictEqual(tenGreenBottles(1))
+    // expect(expect1).toStrictEqual(tenGreenBottles(1))
 })
--- src/tests/Ten Green Bottles/Ten Green Bottles.ts
+++ src/tests/Ten Green Bottles/Ten Green Bottles.ts
@@ -1,4 +1,6 @@
-export function tenGreenBottles(n: number) {
-    const nurseryRhyme = ``;
-    return
+export function tenGreenBottles(n) {
+    const words = (n) => ['no', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',][n];
+
+    const nBottles = n => words(n) + ` green bottle${n === 1 ? '' : 's'}`
+
 }
