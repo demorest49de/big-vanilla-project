9312d82371a98481dea73d4549a00283a6a40dea
Shevchenko Andrey <demorest49de@gmail.com>
2024-02-18 16:47:07 +0300

    Changes Ten Green Bottles.ts

--- src/tests/Ten Green Bottles/Ten Green Bottles.ts
+++ src/tests/Ten Green Bottles/Ten Green Bottles.ts
@@ -1,15 +1,24 @@
 export function tenGreenBottles(n) {
 
-    const words = (n) => ['no', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',][n];
+    const words = (n) => {
+        const temp = ['no', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',][n];
+        return temp;
+    };
+
     const nBottles = n => {
         const temp = words(n) + ` green bottle${n === 1 ? '' : 's'}`;
         return words(n) + ` green bottle${n === 1 ? '' : 's'}`
     }
 
-    const titleCase = word => words(n) + word.slice(1);
+    const titleCase = word => {
+        return words(n) + word.slice(1);
+    }
+
 
     const range = n => {
-        const temp = [...Array(n).keys()].map(x => x + 1).reverse()
+        const temp1 = [...Array(n).keys()]
+        const temp2 = temp1.map(x => x + 1)
+        const temp3 = temp2.reverse()
         return [...Array(n).keys()].map(x => x + 1).reverse();
     }
 
